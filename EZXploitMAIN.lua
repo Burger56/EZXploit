527f2058b6caa0071dc1dd52d950d0a0c31e703f
Burger56 <89709729+Burger56@users.noreply.github.com>
2023-08-14 15:03:24 -0400

    Tiny update

--- EZXploitMAIN.lua
+++ EZXploitMAIN.lua
@@ -1,3 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- Load library
+local Window = Library.CreateLib("EZXploit", "Midnight")
 
-local Window = Library.CreateLib("EZXploit", "Sentinel")
+--MAIN SCRIPTS
+local Tab1 = Window:NewTab("Main")

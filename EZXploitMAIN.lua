87a2af164b289e5cce4041e7f00e185eaa8d5171
Burger56 <89709729+Burger56@users.noreply.github.com>
2023-08-14 14:31:32 -0400

    Delete EZXploitMAIN.lua

--- EZXploitMAIN.lua
+++
@@ -1,3 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- Load library
-
-local Window = Library.CreateLib("EZXploit", "Midnight")

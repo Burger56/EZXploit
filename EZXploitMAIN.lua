ec935a749301211874cf3b6234824e7cbd940340
Burger56 <89709729+Burger56@users.noreply.github.com>
2023-08-14 20:35:51 -0400

    Added some settings and tabs.
    WIP

--- EZXploitMAIN.lua
+++ EZXploitMAIN.lua
@@ -1,12 +1,17 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- Load library
-local colors = {
-    SchemeColor = Color3.fromRGB(255,0,0),
-    Background = Color3.fromRGB(5, 5, 5),
-    Header = Color3.fromRGB(255, 0, 0),
-    TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(0, 255, 0)
-}
+local Window = Library.CreateLib("EZXploit [TESTING]", "Midnight")
 
-local Window = Library.CreateLib("EZXploit", colors)
---MAIN SCRIPTS
-local Tab1 = Window:NewTab("Main")
+-- TABS
+local TabMain = Window:NewTab("Main")
+local TabExperimental = Window:NewTab("Experimental")
+local TabUpdates = Window:NewTab("Update Log")
+local TabSettings = Window:NewTab("Settings")
+
+-- SECTIONS
+
+local ThemeSettings = TabSettings:NewSection("Theme")
+for theme, color in pairs(themes) do
+    Section:NewColorPicker(theme, "Change your "..theme .. "! [WIP]", color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end

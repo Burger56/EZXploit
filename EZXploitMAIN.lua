0616be66c46f4a1c0e19fdf991e44db8a1ed95bd
Burger56 <89709729+Burger56@users.noreply.github.com>
2023-08-14 20:52:06 -0400

    Changed theme colors slightly

--- EZXploitMAIN.lua
+++ EZXploitMAIN.lua
@@ -1,10 +1,10 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- Load library
 local ThemeColors = {
-    SchemeColor = Color3.fromRGB(85, 125, 180),
+    SchemeColor = Color3.fromRGB(135, 185, 200),
     Background = Color3.fromRGB(25, 25, 40),
     Header = Color3.fromRGB(147, 169, 194),
     TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(135, 185, 200)
+    ElementColor = Color3.fromRGB(85, 125, 180)
 }
 local Window = Library.CreateLib("EZXploit [TESTING]", ThemeColors)
 

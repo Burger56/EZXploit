590c029020502267df3a27ee211aa209ffc7faa4
Burger56 <89709729+Burger56@users.noreply.github.com>
2023-08-14 20:47:10 -0400

    Testing new theme

--- EZXploitMAIN.lua
+++ EZXploitMAIN.lua
@@ -1,10 +1,10 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() -- Load library
 local ThemeColors = {
-    SchemeColor = Color3.fromRGB(255,0,0),
-    Background = Color3.fromRGB(5, 5, 8),
-    Header = Color3.fromRGB(0, 255, 0),
+    SchemeColor = Color3.fromRGB(85, 125, 180),
+    Background = Color3.fromRGB(25, 25, 40),
+    Header = Color3.fromRGB(147, 169, 194),
     TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(0, 255, 20)
+    ElementColor = Color3.fromRGB(135, 185, 200)
 }
 local Window = Library.CreateLib("EZXploit [TESTING]", ThemeColors)
 
